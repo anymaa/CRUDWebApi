d7d8ba7bdd7e45f3cf4880790d20fad8998b7d4b
Can Uzun <anymaa@live.com>
2017-06-12 00:41:49 +0300

    Add project files.

---
+++ CRUDWebApi/Controllers/SorunTableController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUDWebApi;
+
+namespace CRUDWebApi.Controllers
+{
+    public class SorunTableController : Controller
+    {
+        private STS2017Entities db = new STS2017Entities();
+
+        // GET: SorunTable
+        public ActionResult Index()
+        {
+            return View(db.TBL_SORUN_.ToList());
+        }
+
+        // GET: SorunTable/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TBL_SORUN_ tBL_SORUN_ = db.TBL_SORUN_.Find(id);
+            if (tBL_SORUN_ == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tBL_SORUN_);
+        }
+
+        // GET: SorunTable/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SorunTable/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Yetkili,Bolge,Aciklama")] TBL_SORUN_ tBL_SORUN_)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TBL_SORUN_.Add(tBL_SORUN_);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tBL_SORUN_);
+        }
+
+        // GET: SorunTable/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TBL_SORUN_ tBL_SORUN_ = db.TBL_SORUN_.Find(id);
+            if (tBL_SORUN_ == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tBL_SORUN_);
+        }
+
+        // POST: SorunTable/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Yetkili,Bolge,Aciklama")] TBL_SORUN_ tBL_SORUN_)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tBL_SORUN_).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tBL_SORUN_);
+        }
+
+        // GET: SorunTable/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TBL_SORUN_ tBL_SORUN_ = db.TBL_SORUN_.Find(id);
+            if (tBL_SORUN_ == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tBL_SORUN_);
+        }
+
+        // POST: SorunTable/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TBL_SORUN_ tBL_SORUN_ = db.TBL_SORUN_.Find(id);
+            db.TBL_SORUN_.Remove(tBL_SORUN_);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CRUDWebApi/Controllers/SorunlarController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CRUDWebApi;
+
+namespace CRUDWebApi.Controllers
+{
+    public class SorunlarController : ApiController
+    {
+        private STS2017Entities db = new STS2017Entities();
+
+        // GET: api/Sorunlar
+        public IQueryable<TBL_SORUN_> GetTBL_SORUN_()
+        {
+            return db.TBL_SORUN_;
+        }
+
+        // GET: api/Sorunlar/5
+        [ResponseType(typeof(TBL_SORUN_))]
+        public IHttpActionResult GetTBL_SORUN_(int id)
+        {
+            TBL_SORUN_ tBL_SORUN_ = db.TBL_SORUN_.Find(id);
+            if (tBL_SORUN_ == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tBL_SORUN_);
+        }
+
+        // PUT: api/Sorunlar/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTBL_SORUN_(int id, TBL_SORUN_ tBL_SORUN_)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != tBL_SORUN_.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(tBL_SORUN_).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TBL_SORUN_Exists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Sorunlar
+        [ResponseType(typeof(TBL_SORUN_))]
+        public IHttpActionResult PostTBL_SORUN_(TBL_SORUN_ tBL_SORUN_)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.TBL_SORUN_.Add(tBL_SORUN_);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (TBL_SORUN_Exists(tBL_SORUN_.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = tBL_SORUN_.Id }, tBL_SORUN_);
+        }
+
+        // DELETE: api/Sorunlar/5
+        [ResponseType(typeof(TBL_SORUN_))]
+        public IHttpActionResult DeleteTBL_SORUN_(int id)
+        {
+            TBL_SORUN_ tBL_SORUN_ = db.TBL_SORUN_.Find(id);
+            if (tBL_SORUN_ == null)
+            {
+                return NotFound();
+            }
+
+            db.TBL_SORUN_.Remove(tBL_SORUN_);
+            db.SaveChanges();
+
+            return Ok(tBL_SORUN_);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TBL_SORUN_Exists(int id)
+        {
+            return db.TBL_SORUN_.Count(e => e.Id == id) > 0;
+        }
+    }
+}
